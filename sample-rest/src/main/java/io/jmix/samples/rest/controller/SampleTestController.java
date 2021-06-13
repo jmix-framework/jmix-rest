d0e42a16fb3860995e9d2da3ef13d1956c520c50
subbotin <a.subbotin@haulmont.com>
2021-06-13 14:52:25 +0400

    Enable OAuth2 security for custom endpoints by default#78

--- sample-rest/src/main/java/io/jmix/samples/rest/controller/SampleTestController.java
+++ sample-rest/src/main/java/io/jmix/samples/rest/controller/SampleTestController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 
 @RestController
-@RequestMapping(value = "/myapi/sample")
+@RequestMapping(value = "/rest/sample")
 public class SampleTestController {
 
     @Autowired
